7f3c39fb0fd3fe8f6266b01a4a1a714e603dc466
Денис Марьянский <d.maryanskiy@gmail.com>
2022-11-01 21:58:12 +0100

    Minor fixes and added description to API.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from routers.routers import api_router
 
 app = FastAPI(
-    title="Pet ToDo List using FastAPI."
+    title="Pet ToDo List using FastAPI.",
     description="As a pet project I decided to develop ToDo List for my own usage."
 )
 
